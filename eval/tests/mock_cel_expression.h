0f4cc138719d57d3ac9fef16bff39407d76fca54
CEL Dev Team <cel-dev@google.com>
2020-09-02 20:57:08 -0400

    Internal cleanup migrating StatusOr.
    PiperOrigin-RevId: 329825560

--- eval/tests/mock_cel_expression.h
+++ eval/tests/mock_cel_expression.h
@@ -17,20 +17,20 @@ class MockCelExpression : public CelExpression {
   MOCK_METHOD(std::unique_ptr<CelEvaluationState>, InitializeState,
               (google::protobuf::Arena * arena), (const, override));
 
-  MOCK_METHOD(::absl::StatusOr<CelValue>, Evaluate,
+  MOCK_METHOD(absl::StatusOr<CelValue>, Evaluate,
               (const BaseActivation& activation, google::protobuf::Arena* arena),
               (const, override));
 
-  MOCK_METHOD(::absl::StatusOr<CelValue>, Evaluate,
+  MOCK_METHOD(absl::StatusOr<CelValue>, Evaluate,
               (const BaseActivation& activation, CelEvaluationState* state),
               (const, override));
 
-  MOCK_METHOD(::absl::StatusOr<CelValue>, Trace,
+  MOCK_METHOD(absl::StatusOr<CelValue>, Trace,
               (const BaseActivation& activation, google::protobuf::Arena* arena,
                CelEvaluationListener callback),
               (const, override));
 
-  MOCK_METHOD(::absl::StatusOr<CelValue>, Trace,
+  MOCK_METHOD(absl::StatusOr<CelValue>, Trace,
               (const BaseActivation& activation, CelEvaluationState* state,
                CelEvaluationListener callback),
               (const, override));
