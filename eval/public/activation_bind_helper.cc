99edfb1859da4444ad0262efba4659475c3ee2c5
CEL Dev Team <cel-dev@google.com>
2021-02-23 07:21:44 -0800

    Return a status if nullptr arena is passed.
    PiperOrigin-RevId: 359041829

--- eval/public/activation_bind_helper.cc
+++ eval/public/activation_bind_helper.cc
@@ -1,5 +1,6 @@
 #include "eval/public/activation_bind_helper.h"
 
+#include "absl/status/status.h"
 #include "eval/public/containers/field_access.h"
 #include "eval/public/containers/field_backed_list_impl.h"
 #include "eval/public/containers/field_backed_map_impl.h"
@@ -37,6 +38,13 @@ absl::Status CreateValueFromField(const google::protobuf::Message* msg,
 absl::Status BindProtoToActivation(const Message* message, Arena* arena,
                                    Activation* activation,
                                    ProtoUnsetFieldOptions options) {
+  // If we need to bind any types that are backed by an arena allocation, we
+  // will cause a memory leak.
+  if (arena == nullptr) {
+    return absl::InvalidArgumentError(
+        "arena must not be null for BindProtoToActivation.");
+  }
+
   // TODO(issues/24): Improve the utilities to bind dynamic values as well.
   const Descriptor* desc = message->GetDescriptor();
   const google::protobuf::Reflection* reflection = message->GetReflection();
