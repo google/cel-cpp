2ddc5b8d347f7efef126f0d5e4153c1aac222c9c
jdtatum <jdtatum@google.com>
2022-11-28 18:58:11 +0000

    Add constant folding benchmarks for expression builder.
    PiperOrigin-RevId: 491389460

--- eval/tests/expression_builder_benchmark_test.cc
+++ eval/tests/expression_builder_benchmark_test.cc
@@ -14,22 +14,18 @@
  * limitations under the License.
  */
 
+#include <cmath>
+#include <string>
+
 #include "google/api/expr/v1alpha1/syntax.pb.h"
 #include "google/protobuf/text_format.h"
-#include "absl/base/attributes.h"
-#include "absl/container/btree_map.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/container/node_hash_set.h"
-#include "absl/strings/match.h"
-#include "eval/public/activation.h"
+#include "absl/strings/str_cat.h"
 #include "eval/public/builtin_func_registrar.h"
 #include "eval/public/cel_expr_builder_factory.h"
 #include "eval/public/cel_expression.h"
 #include "eval/public/cel_options.h"
-#include "eval/public/cel_value.h"
-#include "eval/public/containers/container_backed_list_impl.h"
-#include "eval/public/containers/container_backed_map_impl.h"
-#include "eval/public/structs/cel_proto_wrapper.h"
 #include "eval/tests/request_context.pb.h"
 #include "internal/benchmark.h"
 #include "internal/status_macros.h"
@@ -42,6 +38,8 @@ namespace {
 
 using google::api::expr::v1alpha1::ParsedExpr;
 
+enum BenchmarkParam : int { kDefault = 0, kFoldConstants = 1 };
+
 void BM_RegisterBuiltins(benchmark::State& state) {
   for (auto _ : state) {
     auto builder = CreateCelExpressionBuilder();
@@ -53,6 +51,8 @@ void BM_RegisterBuiltins(benchmark::State& state) {
 BENCHMARK(BM_RegisterBuiltins);
 
 void BM_SymbolicPolicy(benchmark::State& state) {
+  auto param = static_cast<BenchmarkParam>(state.range(0));
+
   ASSERT_OK_AND_ASSIGN(ParsedExpr expr, parser::Parse(R"cel(
    !(request.ip in ["10.0.1.4", "10.0.1.5", "10.0.1.6"]) &&
    ((request.path.startsWith("v1") && request.token in ["v1", "v2", "admin"]) ||
@@ -61,7 +61,19 @@ void BM_SymbolicPolicy(benchmark::State& state) {
      request.ip in ["10.0.1.1",  "10.0.1.2", "10.0.1.3"])
    ))cel"));
 
+  google::protobuf::Arena arena;
   InterpreterOptions options;
+
+  switch (param) {
+    case BenchmarkParam::kFoldConstants:
+      options.constant_arena = &arena;
+      options.constant_folding = true;
+      break;
+    case BenchmarkParam::kDefault:
+      options.constant_folding = false;
+      break;
+  }
+
   auto builder = CreateCelExpressionBuilder(options);
   auto reg_status = RegisterBuiltinFunctions(builder->GetRegistry());
   ASSERT_OK(reg_status);
@@ -73,14 +85,30 @@ void BM_SymbolicPolicy(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_SymbolicPolicy);
+BENCHMARK(BM_SymbolicPolicy)
+    ->Arg(BenchmarkParam::kDefault)
+    ->Arg(BenchmarkParam::kFoldConstants);
 
 void BM_NestedComprehension(benchmark::State& state) {
+  auto param = static_cast<BenchmarkParam>(state.range(0));
+
   ASSERT_OK_AND_ASSIGN(ParsedExpr expr, parser::Parse(R"(
     [4, 5, 6].all(x, [1, 2, 3].all(y, x > y) && [7, 8, 9].all(z, x < z))
   )"));
 
   InterpreterOptions options;
+  google::protobuf::Arena arena;
+
+  switch (param) {
+    case BenchmarkParam::kFoldConstants:
+      options.constant_arena = &arena;
+      options.constant_folding = true;
+      break;
+    case BenchmarkParam::kDefault:
+      options.constant_folding = false;
+      break;
+  }
+
   auto builder = CreateCelExpressionBuilder(options);
   auto reg_status = RegisterBuiltinFunctions(builder->GetRegistry());
   ASSERT_OK(reg_status);
@@ -92,9 +120,13 @@ void BM_NestedComprehension(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_NestedComprehension);
+BENCHMARK(BM_NestedComprehension)
+    ->Arg(BenchmarkParam::kDefault)
+    ->Arg(BenchmarkParam::kFoldConstants);
 
 void BM_Comparisons(benchmark::State& state) {
+  auto param = static_cast<BenchmarkParam>(state.range(0));
+
   ASSERT_OK_AND_ASSIGN(ParsedExpr expr, parser::Parse(R"(
     v11 < v12 && v12 < v13
       && v21 > v22 && v22 > v23
@@ -103,6 +135,18 @@ void BM_Comparisons(benchmark::State& state) {
   )"));
 
   InterpreterOptions options;
+  google::protobuf::Arena arena;
+
+  switch (param) {
+    case BenchmarkParam::kFoldConstants:
+      options.constant_arena = &arena;
+      options.constant_folding = true;
+      break;
+    case BenchmarkParam::kDefault:
+      options.constant_folding = false;
+      break;
+  }
+
   auto builder = CreateCelExpressionBuilder(options);
   auto reg_status = RegisterBuiltinFunctions(builder->GetRegistry());
   ASSERT_OK(reg_status);
@@ -114,7 +158,60 @@ void BM_Comparisons(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_Comparisons);
+BENCHMARK(BM_Comparisons)
+    ->Arg(BenchmarkParam::kDefault)
+    ->Arg(BenchmarkParam::kFoldConstants);
+
+void BM_StringConcat(benchmark::State& state) {
+  auto param = static_cast<BenchmarkParam>(state.range(0));
+  auto size = state.range(1);
+
+  std::string source = "'1234567890' + '1234567890'";
+  auto iter = static_cast<int>(std::log2(size));
+  for (int i = 1; i < iter; i++) {
+    source = absl::StrCat(source, " + ", source);
+  }
+
+  // add a non const branch to the expression.
+  absl::StrAppend(&source, " + identifier");
+
+  ASSERT_OK_AND_ASSIGN(ParsedExpr expr, parser::Parse(source));
+
+  InterpreterOptions options;
+  google::protobuf::Arena arena;
+
+  switch (param) {
+    case BenchmarkParam::kFoldConstants:
+      options.constant_arena = &arena;
+      options.constant_folding = true;
+      break;
+    case BenchmarkParam::kDefault:
+      options.constant_folding = false;
+      break;
+  }
+
+  auto builder = CreateCelExpressionBuilder(options);
+  auto reg_status = RegisterBuiltinFunctions(builder->GetRegistry());
+  ASSERT_OK(reg_status);
+
+  for (auto _ : state) {
+    ASSERT_OK_AND_ASSIGN(
+        auto expression,
+        builder->CreateExpression(&expr.expr(), &expr.source_info()));
+  }
+}
+
+BENCHMARK(BM_StringConcat)
+    ->Args({BenchmarkParam::kDefault, 2})
+    ->Args({BenchmarkParam::kDefault, 4})
+    ->Args({BenchmarkParam::kDefault, 8})
+    ->Args({BenchmarkParam::kDefault, 16})
+    ->Args({BenchmarkParam::kDefault, 32})
+    ->Args({BenchmarkParam::kFoldConstants, 2})
+    ->Args({BenchmarkParam::kFoldConstants, 4})
+    ->Args({BenchmarkParam::kFoldConstants, 8})
+    ->Args({BenchmarkParam::kFoldConstants, 16})
+    ->Args({BenchmarkParam::kFoldConstants, 32});
 
 }  // namespace
 }  // namespace google::api::expr::runtime
