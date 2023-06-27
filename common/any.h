f25f4149713bf86a22ce648db51cce5c07c54df9
Justin King <jcking@google.com>
2023-06-27 12:00:15 -0700

    Add abstract interfaces for generalizing JSON-like and any conversion
    PiperOrigin-RevId: 543812583

---
+++ common/any.h
@@ -0,0 +1,45 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef THIRD_PARTY_CEL_CPP_COMMON_ANY_H_
+#define THIRD_PARTY_CEL_CPP_COMMON_ANY_H_
+
+#include <string>
+#include <utility>
+
+#include "absl/strings/cord.h"
+#include "absl/strings/string_view.h"
+
+namespace cel {
+
+// `Any` is a native C++ representation of `google.protobuf.Any`, without the
+// protocol buffer dependency.
+class Any final {
+ public:
+  void set_type_url(std::string type_url) { type_url_ = std::move(type_url); }
+
+  absl::string_view type_url() const { return type_url_; }
+
+  void set_value(absl::Cord value) { value_ = std::move(value); }
+
+  absl::Cord value() const { return value_; }
+
+ private:
+  std::string type_url_;
+  absl::Cord value_;
+};
+
+}  // namespace cel
+
+#endif  // THIRD_PARTY_CEL_CPP_COMMON_ANY_H_
