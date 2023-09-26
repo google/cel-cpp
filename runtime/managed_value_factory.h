5614f60d60bad3b04d3ab3edbd1136118d62dbfa
Jonathan Tatum <jdtatum@google.com>
2023-09-26 16:27:17 -0700

    Add public interfaces for evaluator using modern types.
    PiperOrigin-RevId: 568680295

---
+++ runtime/managed_value_factory.h
@@ -0,0 +1,52 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef THIRD_PARTY_CEL_CPP_RUNTIME_MANAGED_VALUE_FACTORY_H_
+#define THIRD_PARTY_CEL_CPP_RUNTIME_MANAGED_VALUE_FACTORY_H_
+
+#include "base/memory.h"
+#include "base/type_factory.h"
+#include "base/type_manager.h"
+#include "base/type_provider.h"
+#include "base/value_factory.h"
+
+namespace cel {
+
+// A convenience class for managing objects associated with a ValueFactory.
+class ManagedValueFactory {
+ public:
+  // type_provider and memory_manager must outlive the ManagedValueFactory.
+  ManagedValueFactory(const TypeProvider& type_provider,
+                      MemoryManager& memory_manager)
+      : type_factory_(memory_manager),
+        type_manager_(type_factory_, type_provider),
+        value_factory_(type_manager_) {}
+
+  // Move-only
+  ManagedValueFactory(const ManagedValueFactory& other) = delete;
+  ManagedValueFactory& operator=(const ManagedValueFactory& other) = delete;
+  ManagedValueFactory(ManagedValueFactory&& other) = default;
+  ManagedValueFactory& operator=(ManagedValueFactory&& other) = default;
+
+  ValueFactory& get() { return value_factory_; }
+
+ private:
+  TypeFactory type_factory_;
+  TypeManager type_manager_;
+  ValueFactory value_factory_;
+};
+
+}  // namespace cel
+
+#endif  // THIRD_PARTY_CEL_CPP_RUNTIME_MANAGED_VALUE_FACTORY_H_
---
+++ runtime/runtime.h
@@ -0,0 +1,125 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Interfaces for runtime concepts.
+
+#ifndef THIRD_PARTY_CEL_CPP_RUNTIME_RUNTIME_H_
+#define THIRD_PARTY_CEL_CPP_RUNTIME_RUNTIME_H_
+
+#include <cstdint>
+#include <memory>
+
+#include "absl/functional/any_invocable.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "base/ast.h"
+#include "base/handle.h"
+#include "base/type_provider.h"
+#include "base/value.h"
+#include "base/value_factory.h"
+#include "runtime/activation_interface.h"
+
+namespace cel {
+
+// Representation of an evaluable CEL expression.
+//
+// See Runtime below for creating new programs.
+class Program {
+ public:
+  virtual ~Program() = default;
+
+  // Evaluate the program.
+  //
+  // Non-recoverable errors (i.e. outside of CEL's notion of an error) are
+  // returned as a non-ok absl::Status. These are propagated immediately and do
+  // not participate in CEL's notion of error handling.
+  //
+  // CEL errors are represented as result with an Ok status and a held
+  // cel::ErrorValue result.
+  //
+  // Activation manages instances of variables available in the cel expression's
+  // environment.
+  //
+  // The memory manager determines the lifecycle requirements of the returned
+  // value. The most common choices are:
+  //  - cel::MemoryManager::Global(): created values are allocated on the heap
+  //    and managed by a reference count. Destructor is called when reference
+  //    count is 0.
+  //  - cel::extensions::ProtoMemoryManager instance: created values are
+  //    allocated on the backing protobuf Arena. Destructors for allocated
+  //    objects are called on destruction of the Arena. Note: instances may
+  //    still allocate additional memory on the heap e.g. a vector's storage
+  //    may still be on the global heap.
+  //
+  //  For consistency, users should use the same memory manager to create values
+  //  in the activation and for Program evaluation.
+  virtual absl::StatusOr<Handle<Value>> Evaluate(
+      const ActivationInterface& activation,
+      ValueFactory& value_factory) const = 0;
+
+  virtual const TypeProvider& GetTypeProvider() const = 0;
+};
+
+// Representation for a traceable CEL expression.
+//
+// Implementations provide an additional Trace method that evaluates the
+// expression and invokes a callback allowing callers to inspect intermediate
+// state during evaluation.
+class TraceableProgram : public Program {
+ public:
+  // EvaluationListener may be provided to an EvaluateWithCallback call to
+  // inspect intermediate values during evaluation.
+  //
+  // The callback is called on after every program step that corresponds
+  // to an AST expression node. The value provided is the top of the value
+  // stack, corresponding to the result of evaluating the given sub expression.
+  //
+  // A returning a non-ok status stops evaluation and forwards the error.
+  using EvaluationListener = absl::AnyInvocable<absl::Status(
+      int64_t expr_id, const Handle<Value>&, ValueFactory&)>;
+
+  // Evaluate the Program plan with a Listener.
+  //
+  // The given callback will be invoked after evaluating any program step
+  // that corresponds to an AST node in the planned CEL expression.
+  //
+  // If the callback returns a non-ok status, evaluation stops and the Status
+  // is forwarded as the result of the EvaluateWithCallback call.
+  virtual absl::StatusOr<Handle<Value>> Trace(
+      const ActivationInterface&, EvaluationListener evaluation_listener,
+      ValueFactory& value_factory) const = 0;
+};
+
+// Interface for a CEL runtime.
+//
+// Manages the state necessary to generate Programs.
+//
+// Runtime instances should be created from a RuntimeBuilder rather than
+// instantiated directly.
+class Runtime {
+ public:
+  virtual ~Runtime() = default;
+
+  virtual absl::StatusOr<std::unique_ptr<Program>> CreateProgram(
+      std::unique_ptr<cel::Ast> ast) const = 0;
+
+  virtual absl::StatusOr<std::unique_ptr<TraceableProgram>>
+  CreateTraceableProgram(std::unique_ptr<cel::Ast> ast) const = 0;
+
+  virtual const TypeProvider& GetTypeProvider() const = 0;
+};
+
+}  // namespace cel
+
+#endif  // THIRD_PARTY_CEL_CPP_RUNTIME_RUNTIME_H_
