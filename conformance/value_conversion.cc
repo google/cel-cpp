7c21e8750b20613c21921c625934f81c64f788e6
Jonathan Tatum <jdtatum@google.com>
2023-10-09 13:04:24 -0700

    Add support for conversions for modern value types to the corresponding google::api::expr::Value for container types.
    PiperOrigin-RevId: 572018968

--- conformance/value_conversion.cc
+++ conformance/value_conversion.cc
@@ -14,6 +14,7 @@
 #include "conformance/value_conversion.h"
 
 #include <string>
+#include <utility>
 
 #include "google/api/expr/v1alpha1/value.pb.h"
 #include "google/protobuf/any.pb.h"
@@ -31,9 +32,15 @@
 #include "base/values/double_value.h"
 #include "base/values/duration_value.h"
 #include "base/values/int_value.h"
+#include "base/values/list_value_builder.h"
+#include "base/values/map_value.h"
+#include "base/values/map_value_builder.h"
 #include "base/values/timestamp_value.h"
 #include "base/values/type_value.h"
 #include "base/values/uint_value.h"
+#include "common/any.h"
+#include "extensions/protobuf/struct_value.h"
+#include "extensions/protobuf/value.h"
 #include "internal/proto_time_encoding.h"
 #include "internal/status_macros.h"
 #include "google/protobuf/message.h"
@@ -42,6 +49,8 @@ namespace cel::conformance_internal {
 namespace {
 
 using ConformanceKind = google::api::expr::v1alpha1::Value::KindCase;
+using ConformanceMapValue = google::api::expr::v1alpha1::MapValue;
+using ConformanceListValue = google::api::expr::v1alpha1::ListValue;
 
 std::string ToString(ConformanceKind kind_case) {
   switch (kind_case) {
@@ -91,8 +100,87 @@ absl::StatusOr<Handle<Value>> FromObject(ValueFactory& value_factory,
     }
     return value_factory.CreateTimestampValue(internal::DecodeTime(timestamp));
   }
-  return absl::UnimplementedError(absl::StrCat(
-      "FromConformanceValue: unsupported protobuf Any type: ", any.type_url()));
+
+  return extensions::ProtoValue::Create(value_factory, any);
+}
+
+absl::StatusOr<Handle<MapValue>> MapValueFromConformance(
+    ValueFactory& value_factory, const ConformanceMapValue& map_value) {
+  MapValueBuilder<Value, Value> builder(
+      value_factory, value_factory.type_factory().GetDynType(),
+      value_factory.type_factory().GetDynType());
+  for (const auto& entry : map_value.entries()) {
+    CEL_ASSIGN_OR_RETURN(auto key,
+                         FromConformanceValue(value_factory, entry.key()));
+    CEL_ASSIGN_OR_RETURN(auto value,
+                         FromConformanceValue(value_factory, entry.value()));
+    CEL_RETURN_IF_ERROR(builder.Put(std::move(key), std::move(value)));
+  }
+
+  return std::move(builder).Build();
+}
+
+absl::StatusOr<Handle<ListValue>> ListValueFromConformance(
+    ValueFactory& value_factory, const ConformanceListValue& list_value) {
+  ListValueBuilder<Value> builder(value_factory,
+                                  value_factory.type_factory().GetDynType());
+  for (const auto& elem : list_value.values()) {
+    CEL_ASSIGN_OR_RETURN(auto value, FromConformanceValue(value_factory, elem));
+    CEL_RETURN_IF_ERROR(builder.Add(std::move(value)));
+  }
+
+  return std::move(builder).Build();
+}
+
+absl::StatusOr<ConformanceMapValue> MapValueToConformance(
+    ValueFactory& value_factory, const MapValue& map_value) {
+  ConformanceMapValue result;
+
+  CEL_ASSIGN_OR_RETURN(auto iter, map_value.NewIterator(value_factory));
+
+  while (iter->HasNext()) {
+    CEL_ASSIGN_OR_RETURN(auto key_value, iter->Next());
+    CEL_ASSIGN_OR_RETURN(auto value_value,
+                         map_value.Get(value_factory, key_value));
+
+    CEL_ASSIGN_OR_RETURN(auto key,
+                         ToConformanceValue(value_factory, key_value));
+    CEL_ASSIGN_OR_RETURN(auto value,
+                         ToConformanceValue(value_factory, value_value));
+
+    auto* entry = result.add_entries();
+
+    *entry->mutable_key() = std::move(key);
+    *entry->mutable_value() = std::move(value);
+  }
+
+  return result;
+}
+
+absl::StatusOr<ConformanceListValue> ListValueToConformance(
+    ValueFactory& value_factory, const ListValue& list_value) {
+  ConformanceListValue result;
+
+  CEL_ASSIGN_OR_RETURN(auto iter, list_value.NewIterator(value_factory));
+
+  while (iter->HasNext()) {
+    CEL_ASSIGN_OR_RETURN(auto elem, iter->Next());
+    CEL_ASSIGN_OR_RETURN(*result.add_values(),
+                         ToConformanceValue(value_factory, elem));
+  }
+
+  return result;
+}
+
+absl::StatusOr<google::protobuf::Any> ToProtobufAny(
+    ValueFactory& value_factory, const StructValue& struct_value) {
+  CEL_ASSIGN_OR_RETURN(cel::Any any, struct_value.ConvertToAny(value_factory));
+
+  google::protobuf::Any result;
+  result.set_type_url(any.type_url());
+  result.set_value(std::string(any.value()));
+
+  return result;
 }
 }  // namespace
 
@@ -116,6 +204,10 @@ absl::StatusOr<Handle<Value>> FromConformanceValue(
       return value_factory.GetNullValue();
     case ConformanceKind::kObjectValue:
       return FromObject(value_factory, value.object_value());
+    case ConformanceKind::kMapValue:
+      return MapValueFromConformance(value_factory, value.map_value());
+    case ConformanceKind::kListValue:
+      return ListValueFromConformance(value_factory, value.list_value());
 
     default:
       return absl::UnimplementedError(absl::StrCat(
@@ -163,6 +255,24 @@ absl::StatusOr<google::api::expr::v1alpha1::Value> ToConformanceValue(
       CEL_RETURN_IF_ERROR(internal::EncodeTime(
           value->As<TimestampValue>().value(), &timestamp));
       result.mutable_object_value()->PackFrom(timestamp);
+      break;
+    }
+    case ValueKind::kMap: {
+      CEL_ASSIGN_OR_RETURN(
+          *result.mutable_map_value(),
+          MapValueToConformance(value_factory, value->As<MapValue>()));
+      break;
+    }
+    case ValueKind::kList: {
+      CEL_ASSIGN_OR_RETURN(
+          *result.mutable_list_value(),
+          ListValueToConformance(value_factory, value->As<ListValue>()));
+      break;
+    }
+    case ValueKind::kStruct: {
+      CEL_ASSIGN_OR_RETURN(
+          *result.mutable_object_value(),
+          ToProtobufAny(value_factory, value->As<StructValue>()));
       break;
     }
     default:
