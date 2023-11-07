d2f9f2c6c8405e331d040ecea44c7ecfb2ded479
Justin King <jcking@google.com>
2023-11-07 12:26:04 -0800

    Publish `common/internal`
    PiperOrigin-RevId: 580264262

---
+++ common/internal/reference_count.h
@@ -0,0 +1,290 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This header contains primitives for reference counting, roughly equivalent to
+// the primitives used to implement `std::shared_ptr`. These primitives should
+// not be used directly in most cases, instead `cel::ManagedMemory` should be
+// used instead.
+
+#ifndef THIRD_PARTY_CEL_CPP_COMMON_INTERNAL_REFERENCE_COUNT_H_
+#define THIRD_PARTY_CEL_CPP_COMMON_INTERNAL_REFERENCE_COUNT_H_
+
+#include <atomic>
+#include <cstdint>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+#include "absl/base/attributes.h"
+#include "absl/base/nullability.h"
+#include "absl/log/absl_check.h"
+
+namespace cel::common_internal {
+
+struct AdoptRef final {
+  explicit AdoptRef() = default;
+};
+
+inline constexpr AdoptRef kAdoptRef{};
+
+class ReferenceCount;
+struct ReferenceCountFromThis;
+
+void SetReferenceCountForThat(
+    ReferenceCountFromThis& that,
+    absl::Nullable<ReferenceCount*> refcount) noexcept;
+
+ABSL_ATTRIBUTE_PURE_FUNCTION
+absl::Nullable<ReferenceCount*> GetReferenceCountForThat(
+    const ReferenceCountFromThis& that) noexcept;
+
+// `ReferenceCountFromThis` is similar to `std::enable_shared_from_this`. It
+// allows the derived object to inspect its own reference count. It should not
+// be used directly, but should be used through
+// `cel::EnableManagedMemoryFromThis`.
+struct ReferenceCountFromThis {
+ private:
+  friend void SetReferenceCountForThat(
+      ReferenceCountFromThis& that,
+      absl::Nullable<ReferenceCount*> refcount) noexcept;
+  friend absl::Nullable<ReferenceCount*> GetReferenceCountForThat(
+      const ReferenceCountFromThis& that) noexcept;
+
+  static constexpr uintptr_t kNullPtr = uintptr_t{0};
+  static constexpr uintptr_t kSentinelPtr = ~kNullPtr;
+
+  absl::Nullable<void*> refcount = reinterpret_cast<void*>(kSentinelPtr);
+};
+
+inline void SetReferenceCountForThat(
+    ReferenceCountFromThis& that,
+    absl::Nullable<ReferenceCount*> refcount) noexcept {
+  ABSL_DCHECK_EQ(that.refcount,
+                 reinterpret_cast<void*>(ReferenceCountFromThis::kSentinelPtr));
+  that.refcount = static_cast<void*>(refcount);
+}
+
+inline absl::Nullable<ReferenceCount*> GetReferenceCountForThat(
+    const ReferenceCountFromThis& that) noexcept {
+  ABSL_DCHECK_NE(that.refcount,
+                 reinterpret_cast<void*>(ReferenceCountFromThis::kSentinelPtr));
+  return static_cast<ReferenceCount*>(that.refcount);
+}
+
+void StrongRef(const ReferenceCount& refcount) noexcept;
+
+void StrongRef(absl::Nullable<const ReferenceCount*> refcount) noexcept;
+
+void StrongUnref(const ReferenceCount& refcount) noexcept;
+
+void StrongUnref(absl::Nullable<const ReferenceCount*> refcount) noexcept;
+
+ABSL_MUST_USE_RESULT
+bool StrengthenRef(const ReferenceCount& refcount) noexcept;
+
+ABSL_MUST_USE_RESULT
+bool StrengthenRef(absl::Nullable<const ReferenceCount*> refcount) noexcept;
+
+void WeakRef(const ReferenceCount& refcount) noexcept;
+
+void WeakRef(absl::Nullable<const ReferenceCount*> refcount) noexcept;
+
+void WeakUnref(const ReferenceCount& refcount) noexcept;
+
+void WeakUnref(absl::Nullable<const ReferenceCount*> refcount) noexcept;
+
+ABSL_MUST_USE_RESULT
+bool IsUniqueRef(const ReferenceCount& refcount) noexcept;
+
+ABSL_MUST_USE_RESULT
+bool IsUniqueRef(absl::Nullable<const ReferenceCount*> refcount) noexcept;
+
+ABSL_MUST_USE_RESULT
+bool IsExpiredRef(const ReferenceCount& refcount) noexcept;
+
+ABSL_MUST_USE_RESULT
+bool IsExpiredRef(absl::Nullable<const ReferenceCount*> refcount) noexcept;
+
+// `ReferenceCount` is similar to the control block used by `std::shared_ptr`.
+// It is not meant to be interacted with directly in most cases, instead
+// `cel::ManagedMemory` should be used.
+class ReferenceCount {
+ public:
+  ReferenceCount() = default;
+
+  ReferenceCount(const ReferenceCount&) = delete;
+  ReferenceCount(ReferenceCount&&) = delete;
+  ReferenceCount& operator=(const ReferenceCount&) = delete;
+  ReferenceCount& operator=(ReferenceCount&&) = delete;
+
+  virtual ~ReferenceCount() = default;
+
+ private:
+  friend void StrongRef(const ReferenceCount& refcount) noexcept;
+  friend void StrongUnref(const ReferenceCount& refcount) noexcept;
+  friend bool StrengthenRef(const ReferenceCount& refcount) noexcept;
+  friend void WeakRef(const ReferenceCount& refcount) noexcept;
+  friend void WeakUnref(const ReferenceCount& refcount) noexcept;
+  friend bool IsUniqueRef(const ReferenceCount& refcount) noexcept;
+  friend bool IsExpiredRef(const ReferenceCount& refcount) noexcept;
+
+  virtual void Finalize() const noexcept = 0;
+
+  mutable std::atomic<int32_t> strong_refcount_ = 1;
+  mutable std::atomic<int32_t> weak_refcount_ = 1;
+};
+
+template <typename T>
+class InlinedReferenceCount final : public ReferenceCount {
+ public:
+  template <typename... Args>
+  explicit InlinedReferenceCount(std::in_place_t, Args&&... args)
+      : ReferenceCount() {
+    ::new (static_cast<void*>(value())) T(std::forward<Args>(args)...);
+  }
+
+  ABSL_ATTRIBUTE_ALWAYS_INLINE absl::Nonnull<T*> value() {
+    return reinterpret_cast<T*>(&value_[0]);
+  }
+
+  ABSL_ATTRIBUTE_ALWAYS_INLINE absl::Nonnull<const T*> value() const {
+    return reinterpret_cast<const T*>(&value_[0]);
+  }
+
+ private:
+  void Finalize() const noexcept override { value()->~T(); }
+
+  // We store the instance of `T` in a char buffer and use placement new and
+  // direct calls to the destructor. The reason for this is `Finalize()` is
+  // called when the strong reference count hits 0. This allows us to destroy
+  // our instance of `T` once we are no longer strongly reachable and deallocate
+  // the memory once we are no longer weakly reachable.
+  alignas(T) char value_[sizeof(T)];
+};
+
+template <typename T, typename... Args>
+std::pair<absl::Nonnull<T*>, absl::Nonnull<ReferenceCount*>> MakeReferenceCount(
+    Args&&... args) {
+  using U = std::remove_const_t<T>;
+  auto* const refcount =
+      new InlinedReferenceCount<U>(std::in_place, std::forward<Args>(args)...);
+  auto* const pointer = refcount->value();
+  if constexpr (std::is_base_of_v<ReferenceCountFromThis, U>) {
+    SetReferenceCountForThat(*pointer, refcount);
+  }
+  return std::make_pair(static_cast<T*>(pointer),
+                        static_cast<ReferenceCount*>(refcount));
+}
+
+inline void StrongRef(const ReferenceCount& refcount) noexcept {
+  const auto count =
+      refcount.strong_refcount_.fetch_add(1, std::memory_order_relaxed);
+  ABSL_DCHECK_GT(count, 0);
+}
+
+inline void StrongRef(absl::Nullable<const ReferenceCount*> refcount) noexcept {
+  if (refcount != nullptr) {
+    StrongRef(*refcount);
+  }
+}
+
+inline void StrongUnref(const ReferenceCount& refcount) noexcept {
+  const auto count =
+      refcount.strong_refcount_.fetch_sub(1, std::memory_order_acq_rel);
+  ABSL_DCHECK_GT(count, 0);
+  if (count == 1) {
+    const_cast<ReferenceCount&>(refcount).Finalize();
+    WeakUnref(refcount);
+  }
+}
+
+inline void StrongUnref(
+    absl::Nullable<const ReferenceCount*> refcount) noexcept {
+  if (refcount != nullptr) {
+    StrongUnref(*refcount);
+  }
+}
+
+inline bool StrengthenRef(const ReferenceCount& refcount) noexcept {
+  auto count = refcount.strong_refcount_.load(std::memory_order_relaxed);
+  while (true) {
+    ABSL_DCHECK_GE(count, 0);
+    if (count == 0) {
+      return false;
+    }
+    if (refcount.strong_refcount_.compare_exchange_weak(
+            count, count + 1, std::memory_order_release,
+            std::memory_order_relaxed)) {
+      return true;
+    }
+  }
+}
+
+inline bool StrengthenRef(
+    absl::Nullable<const ReferenceCount*> refcount) noexcept {
+  return refcount != nullptr ? StrengthenRef(*refcount) : false;
+}
+
+inline void WeakRef(const ReferenceCount& refcount) noexcept {
+  const auto count =
+      refcount.weak_refcount_.fetch_add(1, std::memory_order_relaxed);
+  ABSL_DCHECK_GT(count, 0);
+}
+
+inline void WeakRef(absl::Nullable<const ReferenceCount*> refcount) noexcept {
+  if (refcount != nullptr) {
+    WeakRef(*refcount);
+  }
+}
+
+inline void WeakUnref(const ReferenceCount& refcount) noexcept {
+  const auto count =
+      refcount.weak_refcount_.fetch_sub(1, std::memory_order_acq_rel);
+  ABSL_DCHECK_GT(count, 0);
+  if (count == 1) {
+    delete std::addressof(refcount);
+  }
+}
+
+inline void WeakUnref(absl::Nullable<const ReferenceCount*> refcount) noexcept {
+  if (refcount != nullptr) {
+    WeakUnref(*refcount);
+  }
+}
+
+inline bool IsUniqueRef(const ReferenceCount& refcount) noexcept {
+  const auto count = refcount.strong_refcount_.load(std::memory_order_acquire);
+  ABSL_DCHECK_GT(count, 0);
+  return count == 1;
+}
+
+inline bool IsUniqueRef(
+    absl::Nullable<const ReferenceCount*> refcount) noexcept {
+  return refcount != nullptr ? IsUniqueRef(*refcount) : false;
+}
+
+inline bool IsExpiredRef(const ReferenceCount& refcount) noexcept {
+  const auto count = refcount.strong_refcount_.load(std::memory_order_acquire);
+  ABSL_DCHECK_GE(count, 0);
+  return count == 0;
+}
+
+inline bool IsExpiredRef(
+    absl::Nullable<const ReferenceCount*> refcount) noexcept {
+  return refcount != nullptr ? IsExpiredRef(*refcount) : false;
+}
+
+}  // namespace cel::common_internal
+
+#endif  // THIRD_PARTY_CEL_CPP_COMMON_INTERNAL_REFERENCE_COUNT_H_
---
+++ common/internal/shared_byte_string.h
@@ -0,0 +1,373 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef THIRD_PARTY_CEL_CPP_COMMON_INTERNAL_SHARED_BYTE_STRING_H_
+#define THIRD_PARTY_CEL_CPP_COMMON_INTERNAL_SHARED_BYTE_STRING_H_
+
+#include <cstddef>
+#include <string>
+#include <type_traits>
+#include <utility>
+
+#include "absl/base/attributes.h"
+#include "absl/base/casts.h"
+#include "absl/log/absl_check.h"
+#include "absl/strings/cord.h"
+#include "absl/strings/string_view.h"
+#include "common/internal/reference_count.h"
+#include "internal/overloaded.h"
+
+namespace cel::common_internal {
+
+#ifdef _MSC_VER
+#pragma pack(pack, 1)
+#endif
+
+struct ABSL_ATTRIBUTE_PACKED SharedByteStringHeader final {
+  // True if the content is `absl::Cord`.
+  bool is_cord : 1;
+  // Only used when `is_cord` is `false`.
+  size_t size : sizeof(size_t) * 8 - 1;
+
+  SharedByteStringHeader(bool is_cord, size_t size)
+      : is_cord(is_cord), size(size) {
+    // Ensure size does not occupy the most significant bit.
+    ABSL_DCHECK_GE(absl::bit_cast<std::make_signed_t<size_t>>(size), 0);
+  }
+};
+
+#ifdef _MSC_VER
+#pragma pack(pop)
+#endif
+
+static_assert(sizeof(SharedByteStringHeader) == sizeof(size_t));
+
+class SharedByteString;
+class ABSL_ATTRIBUTE_TRIVIAL_ABI SharedByteStringView;
+
+// `SharedByteString` is a compact wrapper around either an `absl::Cord` or
+// `absl::string_view` with `const ReferenceCount*`.
+class SharedByteString final {
+ public:
+  SharedByteString() noexcept : SharedByteString(absl::string_view()) {}
+
+  explicit SharedByteString(absl::string_view string_view) noexcept
+      : SharedByteString(nullptr, string_view) {}
+
+  explicit SharedByteString(
+      const std::string& string ABSL_ATTRIBUTE_LIFETIME_BOUND) noexcept
+      : SharedByteString(absl::string_view(string)) {}
+
+  explicit SharedByteString(std::string&& string)
+      : SharedByteString(absl::Cord(std::move(string))) {}
+
+  // Constructs a `SharedByteString` whose contents are `string_view` owned by
+  // `refcount`. If `refcount` is not nullptr, a strong reference is taken.
+  SharedByteString(const ReferenceCount* refcount,
+                   absl::string_view string_view) noexcept
+      : header_(false, string_view.size()) {
+    content_.string.data = string_view.data();
+    content_.string.refcount = refcount;
+    (StrongRef)(refcount);
+  }
+
+  explicit SharedByteString(absl::Cord cord) noexcept : header_(true, 0) {
+    ::new (static_cast<void*>(cord_ptr())) absl::Cord(std::move(cord));
+  }
+
+  explicit SharedByteString(SharedByteStringView other) noexcept;
+
+  SharedByteString(const SharedByteString& other) noexcept
+      : header_(other.header_) {
+    if (header_.is_cord) {
+      ::new (static_cast<void*>(cord_ptr())) absl::Cord(*other.cord_ptr());
+    } else {
+      content_.string.data = other.content_.string.data;
+      content_.string.refcount = other.content_.string.refcount;
+      (StrongRef)(content_.string.refcount);
+    }
+  }
+
+  SharedByteString(SharedByteString&& other) noexcept : header_(other.header_) {
+    if (header_.is_cord) {
+      ::new (static_cast<void*>(cord_ptr()))
+          absl::Cord(std::move(*other.cord_ptr()));
+    } else {
+      content_.string.data = other.content_.string.data;
+      content_.string.refcount = other.content_.string.refcount;
+      other.content_.string.data = "";
+      other.content_.string.refcount = nullptr;
+      other.header_.size = 0;
+    }
+  }
+
+  ~SharedByteString() noexcept {
+    if (header_.is_cord) {
+      cord_ptr()->~Cord();
+    } else {
+      (StrongUnref)(content_.string.refcount);
+    }
+  }
+
+  SharedByteString& operator=(const SharedByteString& other) noexcept {
+    this->~SharedByteString();
+    ::new (static_cast<void*>(this)) SharedByteString(other);
+    return *this;
+  }
+
+  SharedByteString& operator=(SharedByteString&& other) noexcept {
+    this->~SharedByteString();
+    ::new (static_cast<void*>(this)) SharedByteString(std::move(other));
+    return *this;
+  }
+
+  template <typename Visitor>
+  std::common_type_t<std::invoke_result_t<Visitor, absl::string_view>,
+                     std::invoke_result_t<Visitor, const absl::Cord&>>
+  Visit(Visitor&& visitor) const {
+    if (header_.is_cord) {
+      return std::forward<Visitor>(visitor)(*cord_ptr());
+    } else {
+      return std::forward<Visitor>(visitor)(
+          absl::string_view(content_.string.data, header_.size));
+    }
+  }
+
+  void swap(SharedByteString& other) noexcept {
+    using std::swap;
+    if (header_.is_cord) {
+      // absl::Cord
+      if (other.header_.is_cord) {
+        // absl::Cord
+        swap(*cord_ptr(), *other.cord_ptr());
+      } else {
+        // absl::string_view
+        SwapMixed(*this, other);
+      }
+    } else {
+      // absl::string_view
+      if (other.header_.is_cord) {
+        // absl::Cord
+        SwapMixed(other, *this);
+      } else {
+        // absl::string_view
+        swap(content_.string.data, other.content_.string.data);
+        swap(content_.string.refcount, other.content_.string.refcount);
+      }
+    }
+    swap(header_, other.header_);
+  }
+
+  // Retrieves the contents of this byte string as `absl::string_view`. If this
+  // byte string is backed by an `absl::Cord` which is not flat, `scratch` is
+  // used to store the contents and the returned `absl::string_view` is a view
+  // of `scratch`.
+  absl::string_view ToString(std::string& scratch ABSL_ATTRIBUTE_LIFETIME_BOUND)
+      const ABSL_ATTRIBUTE_LIFETIME_BOUND {
+    return Visit(internal::Overloaded{
+        [](absl::string_view string) -> absl::string_view { return string; },
+        [&scratch](const absl::Cord& cord) -> absl::string_view {
+          if (auto flat = cord.TryFlat(); flat.has_value()) {
+            return *flat;
+          }
+          scratch = static_cast<std::string>(cord);
+          return absl::string_view(scratch);
+        }});
+  }
+
+  std::string ToString() const {
+    return Visit(
+        internal::Overloaded{[](absl::string_view string) -> std::string {
+                               return std::string(string);
+                             },
+                             [](const absl::Cord& cord) -> std::string {
+                               return static_cast<std::string>(cord);
+                             }});
+  }
+
+  absl::Cord ToCord() const {
+    return Visit(internal::Overloaded{
+        [this](absl::string_view string) -> absl::Cord {
+          const auto* refcount = content_.string.refcount;
+          if (refcount != nullptr) {
+            (StrongRef)(*refcount);
+            return absl::MakeCordFromExternal(
+                string, [refcount]() { (StrongUnref)(*refcount); });
+          }
+          return absl::Cord(string);
+        },
+        [](const absl::Cord& cord) -> absl::Cord { return cord; }});
+  }
+
+ private:
+  friend class SharedByteStringView;
+
+  static void SwapMixed(SharedByteString& cord,
+                        SharedByteString& string) noexcept {
+    const auto* string_data = string.content_.string.data;
+    const auto* string_refcount = string.content_.string.refcount;
+    ::new (static_cast<void*>(string.cord_ptr()))
+        absl::Cord(std::move(*cord.cord_ptr()));
+    cord.cord_ptr()->~Cord();
+    cord.content_.string.data = string_data;
+    cord.content_.string.refcount = string_refcount;
+  }
+
+  absl::Cord* cord_ptr() noexcept {
+    return reinterpret_cast<absl::Cord*>(&content_.cord[0]);
+  }
+
+  const absl::Cord* cord_ptr() const noexcept {
+    return reinterpret_cast<const absl::Cord*>(&content_.cord[0]);
+  }
+
+  SharedByteStringHeader header_;
+  union {
+    struct {
+      const char* data;
+      const ReferenceCount* refcount;
+    } string;
+    alignas(absl::Cord) char cord[sizeof(absl::Cord)];
+  } content_;
+};
+
+inline void swap(SharedByteString& lhs, SharedByteString& rhs) noexcept {
+  lhs.swap(rhs);
+}
+
+class ABSL_ATTRIBUTE_TRIVIAL_ABI SharedByteStringView final {
+ public:
+  SharedByteStringView() noexcept : SharedByteStringView(absl::string_view()) {}
+
+  explicit SharedByteStringView(absl::string_view string) noexcept
+      : SharedByteStringView(nullptr, string) {}
+
+  explicit SharedByteStringView(
+      const std::string& string ABSL_ATTRIBUTE_LIFETIME_BOUND) noexcept
+      : SharedByteStringView(absl::string_view(string)) {}
+
+  SharedByteStringView(const ReferenceCount* refcount,
+                       absl::string_view string) noexcept
+      : header_(false, string.size()) {
+    content_.string.data = string.data();
+    content_.string.refcount = refcount;
+  }
+
+  explicit SharedByteStringView(
+      const absl::Cord& cord ABSL_ATTRIBUTE_LIFETIME_BOUND) noexcept
+      : header_(true, 0) {
+    content_.cord = &cord;
+  }
+
+  // NOLINTNEXTLINE(google-explicit-constructor)
+  SharedByteStringView(
+      const SharedByteString& other ABSL_ATTRIBUTE_LIFETIME_BOUND) noexcept
+      : header_(other.header_) {
+    if (header_.is_cord) {
+      content_.cord = other.cord_ptr();
+    } else {
+      content_.string.data = other.content_.string.data;
+      content_.string.refcount = other.content_.string.refcount;
+    }
+  }
+
+  SharedByteStringView(const SharedByteStringView&) = default;
+  SharedByteStringView& operator=(const SharedByteStringView&) = default;
+
+  template <typename Visitor>
+  std::common_type_t<std::invoke_result_t<Visitor, absl::string_view>,
+                     std::invoke_result_t<Visitor, const absl::Cord&>>
+  Visit(Visitor&& visitor) const {
+    if (header_.is_cord) {
+      return std::forward<Visitor>(visitor)(*content_.cord);
+    } else {
+      return std::forward<Visitor>(visitor)(
+          absl::string_view(content_.string.data, header_.size));
+    }
+  }
+
+  void swap(SharedByteStringView& other) noexcept {
+    using std::swap;
+    swap(header_, other.header_);
+    swap(content_, other.content_);
+  }
+
+  // Retrieves the contents of this byte string as `absl::string_view`. If this
+  // byte string is backed by an `absl::Cord` which is not flat, `scratch` is
+  // used to store the contents and the returned `absl::string_view` is a view
+  // of `scratch`.
+  absl::string_view ToString(std::string& scratch ABSL_ATTRIBUTE_LIFETIME_BOUND)
+      const ABSL_ATTRIBUTE_LIFETIME_BOUND {
+    return Visit(internal::Overloaded{
+        [](absl::string_view string) -> absl::string_view { return string; },
+        [&scratch](const absl::Cord& cord) -> absl::string_view {
+          if (auto flat = cord.TryFlat(); flat.has_value()) {
+            return *flat;
+          }
+          scratch = static_cast<std::string>(cord);
+          return absl::string_view(scratch);
+        }});
+  }
+
+  std::string ToString() const {
+    return Visit(
+        internal::Overloaded{[](absl::string_view string) -> std::string {
+                               return std::string(string);
+                             },
+                             [](const absl::Cord& cord) -> std::string {
+                               return static_cast<std::string>(cord);
+                             }});
+  }
+
+  absl::Cord ToCord() const {
+    return Visit(internal::Overloaded{
+        [this](absl::string_view string) -> absl::Cord {
+          const auto* refcount = content_.string.refcount;
+          if (refcount != nullptr) {
+            (StrongRef)(*refcount);
+            return absl::MakeCordFromExternal(
+                string, [refcount]() { (StrongUnref)(*refcount); });
+          }
+          return absl::Cord(string);
+        },
+        [](const absl::Cord& cord) -> absl::Cord { return cord; }});
+  }
+
+ private:
+  friend class SharedByteString;
+
+  SharedByteStringHeader header_;
+  union {
+    struct {
+      const char* data;
+      const ReferenceCount* refcount;
+    } string;
+    const absl::Cord* cord;
+  } content_;
+};
+
+inline SharedByteString::SharedByteString(SharedByteStringView other) noexcept
+    : header_(other.header_) {
+  if (header_.is_cord) {
+    ::new (static_cast<void*>(cord_ptr())) absl::Cord(*other.content_.cord);
+  } else {
+    content_.string.data = other.content_.string.data;
+    content_.string.refcount = other.content_.string.refcount;
+    (StrongRef)(content_.string.refcount);
+  }
+}
+
+}  // namespace cel::common_internal
+
+#endif  // THIRD_PARTY_CEL_CPP_COMMON_INTERNAL_SHARED_BYTE_STRING_H_
