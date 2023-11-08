24446bfd1d6e8466fdbc435aafaca515cbcabb24
Jonathan Tatum <jdtatum@google.com>
2023-11-07 18:53:37 -0800

    Move EqualsProto shim to internal/.
    PiperOrigin-RevId: 580368656

--- testutil/util.h
+++ testutil/util.h
@@ -15,134 +15,13 @@
 #ifndef THIRD_PARTY_CEL_CPP_TESTUTIL_UTIL_H_
 #define THIRD_PARTY_CEL_CPP_TESTUTIL_UTIL_H_
 
-#include <memory>
-#include <ostream>
-#include <string>
-
-#include "absl/log/absl_check.h"
-#include "absl/memory/memory.h"
-#include "absl/strings/string_view.h"
-#include "internal/casts.h"
-#include "internal/testing.h"
-#include "google/protobuf/message.h"
-#include "google/protobuf/message_lite.h"
-#include "google/protobuf/text_format.h"
-#include "google/protobuf/util/message_differencer.h"
-
-namespace cel::internal::test {
-
-/**
- * Simple implementation of a proto matcher comparing string representations.
- *
- * IMPORTANT: Only use this for protos whose textual representation is
- * deterministic (that may not be the case for the map collection type).
- */
-class TextProtoMatcher {
- public:
-  explicit inline TextProtoMatcher(absl::string_view expected)
-      : expected_(expected) {}
-
-  bool MatchAndExplain(const google::protobuf::MessageLite& p,
-                       ::testing::MatchResultListener* listener) const {
-    return MatchAndExplain(cel::internal::down_cast<const google::protobuf::Message&>(p),
-                           listener);
-  }
-
-  bool MatchAndExplain(const google::protobuf::MessageLite* p,
-                       ::testing::MatchResultListener* listener) const {
-    return MatchAndExplain(cel::internal::down_cast<const google::protobuf::Message*>(p),
-                           listener);
-  }
-
-  bool MatchAndExplain(const google::protobuf::Message& p,
-                       ::testing::MatchResultListener* listener) const {
-    auto message = absl::WrapUnique(p.New());
-    ABSL_CHECK(google::protobuf::TextFormat::ParseFromString(expected_, message.get()));
-    return google::protobuf::util::MessageDifferencer::Equals(
-        *message, cel::internal::down_cast<const google::protobuf::Message&>(p));
-  }
-
-  bool MatchAndExplain(const google::protobuf::Message* p,
-                       ::testing::MatchResultListener* listener) const {
-    auto message = absl::WrapUnique(p->New());
-    ABSL_CHECK(google::protobuf::TextFormat::ParseFromString(expected_, message.get()));
-    return google::protobuf::util::MessageDifferencer::Equals(
-        *message, cel::internal::down_cast<const google::protobuf::Message&>(*p));
-  }
-
-  inline void DescribeTo(::std::ostream* os) const { *os << expected_; }
-  inline void DescribeNegationTo(::std::ostream* os) const {
-    *os << "not equal to expected message: " << expected_;
-  }
-
- private:
-  const std::string expected_;
-};
-
-/**
- * Simple implementation of a proto matcher comparing string representations.
- *
- * IMPORTANT: Only use this for protos whose textual representation is
- * deterministic (that may not be the case for the map collection type).
- */
-class ProtoMatcher {
- public:
-  explicit inline ProtoMatcher(const google::protobuf::Message& expected)
-      : expected_(expected.New()) {
-    expected_->CopyFrom(expected);
-  }
-
-  bool MatchAndExplain(const google::protobuf::MessageLite& p,
-                       ::testing::MatchResultListener* listener) const {
-    return MatchAndExplain(cel::internal::down_cast<const google::protobuf::Message&>(p),
-                           listener);
-  }
-
-  bool MatchAndExplain(const google::protobuf::MessageLite* p,
-                       ::testing::MatchResultListener* listener) const {
-    return MatchAndExplain(cel::internal::down_cast<const google::protobuf::Message*>(p),
-                           listener);
-  }
-
-  bool MatchAndExplain(const google::protobuf::Message& p,
-                       ::testing::MatchResultListener* /* listener */) const {
-    return google::protobuf::util::MessageDifferencer::Equals(*expected_, p);
-  }
-
-  bool MatchAndExplain(const google::protobuf::Message* p,
-                       ::testing::MatchResultListener* /* listener */) const {
-    return google::protobuf::util::MessageDifferencer::Equals(*expected_, *p);
-  }
-
-  inline void DescribeTo(::std::ostream* os) const {
-    *os << expected_->DebugString();
-  }
-  inline void DescribeNegationTo(::std::ostream* os) const {
-    *os << "not equal to expected message: " << expected_->DebugString();
-  }
-
- private:
-  std::shared_ptr<google::protobuf::Message> expected_;
-};
-
-// Polymorphic matcher to compare any two protos.
-inline ::testing::PolymorphicMatcher<TextProtoMatcher> EqualsProto(
-    absl::string_view x) {
-  return ::testing::MakePolymorphicMatcher(TextProtoMatcher(x));
-}
-
-// Polymorphic matcher to compare any two protos.
-inline ::testing::PolymorphicMatcher<ProtoMatcher> EqualsProto(
-    const google::protobuf::Message& x) {
-  return ::testing::MakePolymorphicMatcher(ProtoMatcher(x));
-}
-
-}  // namespace cel::internal::test
+#include "internal/proto_matchers.h"
 
 namespace google::api::expr::testutil {
 
-// aliases for old namespace
-using cel::internal::test::EqualsProto;
+// alias for old namespace
+// prefer using cel::internal::test::EqualsProto.
+using ::cel::internal::test::EqualsProto;
 
 }  // namespace google::api::expr::testutil
 
