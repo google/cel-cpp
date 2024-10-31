c7264239a14ce8bea60bf4ddf503ac004c75985a
Justin King <jcking@google.com>
2024-10-31 11:26:36 -0700

    Deprecate the `CreateCelExpressionBuilder` overload that defaults to the generated descriptor pool
    PiperOrigin-RevId: 691875330

--- eval/public/cel_expr_builder_factory.h
+++ eval/public/cel_expr_builder_factory.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 
+#include "absl/base/attributes.h"
 #include "eval/public/cel_expression.h"
 #include "eval/public/cel_options.h"
 #include "google/protobuf/descriptor.h"
@@ -19,6 +20,14 @@ std::unique_ptr<CelExpressionBuilder> CreateCelExpressionBuilder(
     google::protobuf::MessageFactory* message_factory,
     const InterpreterOptions& options = InterpreterOptions());
 
+ABSL_DEPRECATED(
+    "This overload uses the generated descriptor pool, which allows "
+    "expressions to create any messages linked into the binary. This is not "
+    "hermetic and potentially dangerous, you should select the descriptor pool "
+    "carefully. Use the other overload and explicitly pass your descriptor "
+    "pool. It can still be the generated descriptor pool, but the choice "
+    "should be explicit. If you do not need struct creation, use "
+    "`cel::GetMinimalDescriptorPool()`.")
 inline std::unique_ptr<CelExpressionBuilder> CreateCelExpressionBuilder(
     const InterpreterOptions& options = InterpreterOptions()) {
   return CreateCelExpressionBuilder(google::protobuf::DescriptorPool::generated_pool(),
