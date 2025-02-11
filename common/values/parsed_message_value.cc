59e3be6c3c0d886fd6e9672c0a908a26ec5d6a25
CEL Dev Team <cel-dev@google.com>
2025-02-10 18:40:14 -0800

    No public description
    PiperOrigin-RevId: 725427477

--- common/values/parsed_message_value.cc
+++ common/values/parsed_message_value.cc
@@ -83,7 +83,7 @@ absl::Status ParsedMessageValue::SerializeTo(
     return absl::OkStatus();
   }
 
-  if (!value_->SerializePartialToCord(&value)) {
+  if (!value_->SerializePartialToString(&value)) {
     return absl::UnknownError(
         absl::StrCat("failed to serialize message: ", value_->GetTypeName()));
   }
