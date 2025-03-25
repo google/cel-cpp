fa9554bb7d75cddf5ab3de4b340bc7533eaa2420
Justin King <jcking@google.com>
2025-03-25 07:24:35 -0700

    Redo `ArenaString`
    PiperOrigin-RevId: 740339211

--- internal/string_pool.cc
+++ internal/string_pool.cc
@@ -14,10 +14,8 @@
 
 #include "internal/string_pool.h"
 
-#include <cstring>  // IWYU pragma: keep
-#include <string>   // IWYU pragma: keep
+#include <cstring>
 
-#include "absl/base/optimization.h"
 #include "absl/strings/string_view.h"
 #include "google/protobuf/arena.h"
 
@@ -28,8 +26,8 @@ absl::string_view StringPool::InternString(absl::string_view string) {
     return "";
   }
   return *strings_.lazy_emplace(string, [&](const auto& ctor) {
-    ABSL_ASSUME(arena_ != nullptr);
-    char* data = google::protobuf::Arena::CreateArray<char>(arena_, string.size());
+    char* data =
+        reinterpret_cast<char*>(arena()->AllocateAligned(string.size()));
     std::memcpy(data, string.data(), string.size());
     ctor(absl::string_view(data, string.size()));
   });
