e6d4a233ff209678557701e7648497f35a8ea6fd
Jonathan Tatum <jdtatum@google.com>
2025-12-02 16:31:54 -0800

    Remove unused const step factory.
    This overload is never used outside of tests after refactors.
    
    PiperOrigin-RevId: 839472867

--- eval/eval/const_value_step.h
+++ eval/eval/const_value_step.h
@@ -3,29 +3,28 @@
 
 #include <cstdint>
 #include <memory>
+#include <utility>
 
 #include "absl/status/statusor.h"
-#include "common/allocator.h"
-#include "common/constant.h"
 #include "common/value.h"
+#include "eval/eval/compiler_constant_step.h"
 #include "eval/eval/direct_expression_step.h"
 #include "eval/eval/evaluator_core.h"
 
 namespace google::api::expr::runtime {
 
-std::unique_ptr<DirectExpressionStep> CreateConstValueDirectStep(
-    cel::Value value, int64_t expr_id = -1);
-
-// Factory method for Constant Value expression step.
-absl::StatusOr<std::unique_ptr<ExpressionStep>> CreateConstValueStep(
-    cel::Value value, int64_t expr_id, bool comes_from_ast = true);
+// Factory method for Constant AST node expression recursive step.
+inline std::unique_ptr<DirectExpressionStep> CreateConstValueDirectStep(
+    cel::Value value, int64_t id = -1) {
+  return std::make_unique<DirectCompilerConstantStep>(std::move(value), id);
+}
 
 // Factory method for Constant AST node expression step.
-// Copies the Constant Expr node to avoid lifecycle dependency on source
-// expression.
-absl::StatusOr<std::unique_ptr<ExpressionStep>> CreateConstValueStep(
-    const cel::Constant&, int64_t expr_id, cel::Allocator<> allocator,
-    bool comes_from_ast = true);
+inline absl::StatusOr<std::unique_ptr<ExpressionStep>> CreateConstValueStep(
+    cel::Value value, int64_t expr_id, bool comes_from_ast = true) {
+  return std::make_unique<CompilerConstantStep>(std::move(value), expr_id,
+                                                comes_from_ast);
+}
 
 }  // namespace google::api::expr::runtime
 
