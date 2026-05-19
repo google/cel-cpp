2d3f4e047a02f11ff85e20d615fc65f56bab4793
CEL Dev Team <cel-dev@google.com>
2026-05-19 00:18:20 -0700

    No public description
    PiperOrigin-RevId: 917654317

--- common/ast_rewrite_test.cc
+++ common/ast_rewrite_test.cc
@@ -525,7 +525,7 @@ class RewriterExample : public AstRewriterBase {
 
  private:
   absl::Span<const Expr*> path_;
-  absl::optional<int64_t> target_;
+  std::optional<int64_t> target_;
 };
 
 TEST(AstRewrite, SelectRewriteExample) {
